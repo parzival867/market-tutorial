e52a74ad557122c5748ab5aceb90181a3c71266a
parzival867 <nathyadiah@gmail.com>
2021-06-15 21:36:51 -0400

    Add code to verify user login using flask_login and UserMixin

--- market/__init__.py
+++ market/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = 'dev'
 # how to use db.init_app(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from . import routes
