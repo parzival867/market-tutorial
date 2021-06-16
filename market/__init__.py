02a3d8710e30a246b77eb88eeb584f7dd38bc48d
parzival867 <nathyadiah@gmail.com>
2021-06-16 06:22:32 -0400

    Add login required decorator to market page route and code to __init__.py to set up login manager

--- market/__init__.py
+++ market/__init__.py
@@ -11,5 +11,7 @@ app.config['SECRET_KEY'] = 'dev'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from . import routes
--- market/routes.py
+++ market/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from . import db
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
@@ -11,6 +11,7 @@ def home_page():
 	return render_template('home.html')
 
 @app.route('/market')
+@login_required
 def market_page():
 	items = Item.query.all()
 	return render_template('market.html', items=items)
@@ -24,6 +25,8 @@ def register_page():
 				      password=form.password1.data)
 		db.session.add(user_to_create)
 		db.session.commit()
+		login_user(user_to_create)
+		flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category="success")
 		return redirect(url_for('market_page'))
 	if form.errors != {}: #if there are not errors from the validation
 		for err_msg in form.errors.values():
