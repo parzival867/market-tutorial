9113be3514d76570b695776e24ceab9fc9539ce8
parzival867 <nathyadiah@gmail.com>
2021-06-15 20:53:27 -0400

    Add code to hash passwords before entering into system using flask_bcrypt

--- market/__init__.py
+++ market/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
@@ -7,5 +8,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'dev'
 # how to use db.init_app(app)
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from . import routes
